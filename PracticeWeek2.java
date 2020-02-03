96af49db682076bdaf6bf9e1709c43ef8fc3e3e2
marcus021 <60441129+marcus021@users.noreply.github.com>
2020-02-03 12:34:58 -0500

    Add files via upload

---
+++ PracticeWeek2.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @author Marcus
+ * Created 2.2.20
+ * description: Variable Practice 
+ **/
+public class PracticeWeek2 {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+     // Defining variables using double byte and int
+	 double bag = 10.35; 
+	 byte house = 5;
+	 int num = 3;
+	 
+	 // Declaring Variables 
+	 String name1;
+     String name2;
+	 String name3;
+	 
+	 //Assigning Values
+	name1 = "John";
+	name2 = "Mary";
+	name3 = "Jessie";
+	
+	// printing 
+	System.out.println(name1);
+	System.out.println(name2);
+	System.out.println(name3);
+	System.out.println(name1+name2); // Expected: JohnMary
+	}
+}
